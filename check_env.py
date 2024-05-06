6047fff663f5c373f9aaa4e24707fb3ffff09efb
Rushiraj Gadhvi <90374468+gadhvirushiraj@users.noreply.github.com>
2024-05-07 01:01:41 +0530

    env with gym working
    spandan, divita worked on it tooooo

---
+++ check_env.py
@@ -0,0 +1,17 @@
+from stable_baselines3.common.env_checker import check_env
+from env import VortexENV
+
+# check env thorugh stable baselines
+env = VortexENV()
+# check_env(env)
+
+# check env again !
+episodes = 50
+for episode in range(episodes):
+	done = False
+	obs = env.reset()
+	while not done:
+		random_action = env.action_space.sample()
+		print("action selected: ", random_action)
+		obs, reward, done, trun, info = env.step(random_action)
+		print("reward: ", reward)
--- env.py
+++ env.py
@@ -3,10 +3,18 @@ import sys
 import random
 import math
 import numpy as np
+from gym import spaces
+import gym
 
-class VortexStreetSimulation:
-    def __init__(self, width=400, height=200, radius=30):
+class VortexENV(gym.Env):
+    def __init__(self, width=400, height=200, radius=40):
+        
+        super(VortexENV, self).__init__()
         pygame.init()
+
+        # gym init
+        self.action_space = spaces.Box(low=np.array([0]), high=np.array([360]), dtype=np.float32)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(4,), dtype=np.float32)
         
         # Constants
         self.WIDTH = width
@@ -20,9 +28,10 @@ class VortexStreetSimulation:
         self.GREEN = (0, 255, 0)
         self.BACKGROUND_COLOR = (255, 255, 255)
         self.BLUE = (0, 0, 255)
+        self.RED = (255, 0, 0)
         self.BLACK = (0, 0, 0)
 
-        # Screen setup
+        # screen setup
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Von Kármán Vortex Street Simulation")
 
@@ -36,12 +45,92 @@ class VortexStreetSimulation:
         self.cxs = np.array([0, 0, 1, 1, 1, 0, -1, -1, -1])
         self.cys = np.array([0, 1, 1, 0, -1, -1, -1, 0, 1])
         self.weights = np.array([4/9, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36])
+        
 
         self.init_simulation()
+        self.update_flow_field()
+
+    def reset(self):
+        self.done = False
+        self.v = 0.096
+        self.x, self.y = self.spawn_boat()
+        self.target_x, self.target_y = self.spawn_target()
+        self.boat_theta = self.action_space.sample()
+        self.v_x = np.sin(np.deg2rad(self.boat_theta)) * self.v
+        self.v_y = np.cos(np.deg2rad(self.boat_theta)) * self.v
+        
+        rel_goal_x = self.target_x - self.x
+        rel_goal_y = self.target_y - self.y
+        self.observation = [rel_goal_x, rel_goal_y, self.v_x, self.v_y]
+
+        return self.observation, {}
+    
+    def step(self, angle_degrees):
+        angle_radians = math.radians(angle_degrees)
+
+        # Update velocities based on the angle and speed
+        new_v_x = self.v * math.cos(angle_radians)
+        new_v_y = self.v * math.sin(angle_radians)
+        
+        vel_x = self.ux + new_v_x
+        vel_y = self.uy + new_v_y
+
+        # Store previous position of point
+        previous_position = np.array([self.x, self.y])
+        
+        # Update the point's position
+        self.x += vel_x
+        self.y += vel_y
+        
+        # Update the flow
+        self.update_flow_field()
+        
+        # store current position of point
+        current_position = np.array([self.x, self.y])
+        
+        # store target postion as an array
+        target_position = np.array([self.target_x, self.target_y])
+        
+        # Relative distance for reward
+        distance_previous = np.linalg.norm(previous_position - target_position)
+        distance_current = np.linalg.norm(current_position - target_position)
+        
+        # getting relative position from target
+        delta_x = self.target_x - self.x
+        delta_y = self.target_y - self.y
+        
+        # getting the background velocity at the new position
+        bg_vel_x = self.ux[int(self.x)]
+        bg_vel_y = self.uy[int(self.y)]
+        
+        observation = (delta_x, delta_y, bg_vel_x, bg_vel_y)
+        
+        # Termination check
+        self.done, target_reached = self.check_terminal_state()
+        
+        # Reward
+        reward = -1 + 10 * ((distance_previous - distance_current) / self.v) + \
+            200 if target_reached else 0
+
+        # Return the changes in position and the velocities
+        return observation, reward, self.done, {}
+
+    def check_terminal_state(self):
+        # Check if the point is out of bounds
+        if self.x < 0 or self.x >= self.WIDTH or self.y < 0 or self.y >= self.HEIGHT:
+            return True, False
+        # Check collision with the cylinder
+        if (self.x - self.Nx / 4)**2 + (self.y - self.Ny / 2)**2 < (self.Ny / 4)**2:
+            return True, False
+        # Check if the point reaches the traget region
+        if (self.x - self.target_x)**2 + (self.y - self.target_y)**2 < (self.RADIUS / 3)**2:
+            return True, True
+    
+        return False, False
 
     def init_simulation(self):
         self.F = np.ones((self.Ny, self.Nx, self.NL))  # Distribution function
-        self.F += 0.01 * np.random.randn(self.Ny, self.Nx, self.NL)
+        # self.F += 0.01 * np.random.randn(self.Ny, self.Nx, self.NL)
         self.F[:, :, 3] = 2.3  # Adjust initial distribution
         self.rho = np.sum(self.F, 2)
         for i in self.idxs:
@@ -50,17 +139,24 @@ class VortexStreetSimulation:
         self.cylinder = (X - self.Nx // 4) ** 2 + (Y - self.Ny // 2) ** 2 < (self.Ny // 4) ** 2
         self.orange_circle_y = self.HEIGHT // 4
         self.green_circle_y = 3 * self.HEIGHT // 4
-        self.x, self.y = self.random_point_in_green_circle()
-        self.v_x = -0.05
-        self.v_y = -0.1
+        self.reset()
 
-    def random_point_in_green_circle(self):
+    def spawn_boat(self):
         angle = random.uniform(0, 2 * math.pi)
         distance = random.uniform(0, self.RADIUS)
         offset_x = distance * math.cos(angle)
         offset_y = distance * math.sin(angle)
         x = self.CIRCLE_X + offset_x
         y = self.green_circle_y + offset_y
+        return x, y
+    
+    def spawn_target(self):
+        angle = random.uniform(0, 2 * math.pi)
+        distance = random.uniform(0, 2*self.RADIUS/3)
+        offset_x = distance * math.cos(angle)
+        offset_y = distance * math.sin(angle)
+        x = self.CIRCLE_X + offset_x
+        y = self.orange_circle_y + offset_y
         return x, y
 
     def update_flow_field(self):
@@ -71,21 +167,20 @@ class VortexStreetSimulation:
         bndryF = bndryF[:, [0, 5, 6, 7, 8, 1, 2, 3, 4]]
         self.F[self.cylinder, :] = bndryF
         rho = np.sum(self.F, 2)
-        ux = np.sum(self.F * self.cxs, 2) / rho
-        uy = np.sum(self.F * self.cys, 2) / rho
+        self.ux = np.sum(self.F * self.cxs, 2) / rho
+        self.uy = np.sum(self.F * self.cys, 2) / rho
         Feq = np.zeros(self.F.shape)
         for i, cx, cy, w in zip(self.idxs, self.cxs, self.cys, self.weights):
-            Feq[:, :, i] = rho * w * (1 + 3 * (cx * ux + cy * uy) + 9 * (cx * ux + cy * uy)**2 / 2 - 3 * (ux**2 + uy**2) / 2)
+            Feq[:, :, i] = rho * w * (1 + 3 * (cx * self.ux + cy * self.uy) + 9 * (cx * self.ux + cy * self.uy)**2 / 2 - 3 * (self.ux**2 + self.uy**2) / 2)
         self.F += -(1.0 / self.tau) * (self.F - Feq)
-        return ux, uy
 
-    def draw_quiver(self, ux, uy):
+    def draw_quiver(self):
         grid_size = 10
         for row in range(0, self.Ny, grid_size):
             for col in range(0, self.Nx, grid_size):
                 center_x, center_y = col + grid_size // 2, row + grid_size // 2
-                avg_ux = np.mean(ux[row:row + grid_size, col:col + grid_size])
-                avg_uy = np.mean(uy[row:row + grid_size, col:col + grid_size])
+                avg_ux = np.mean(self.ux[row:row + grid_size, col:col + grid_size])
+                avg_uy = np.mean(self.uy[row:row + grid_size, col:col + grid_size])
                 if avg_ux != 0 or avg_uy != 0:
                     vector_length = math.sqrt(avg_ux**2 + avg_uy**2)
                     scaled_ux = avg_ux / vector_length * 10
@@ -106,30 +201,46 @@ class VortexStreetSimulation:
                     pygame.draw.line(self.screen, self.BLUE, (end_x, end_y), (left_arrow_x, left_arrow_y), 2)
                     pygame.draw.line(self.screen, self.BLUE, (end_x, end_y), (right_arrow_x, right_arrow_y), 2)
 
+    def render(self):
 
-    def run(self):
+        self.draw_quiver()
+        pygame.draw.circle(self.screen, self.BLACK, (self.Nx // 4, self.Ny // 2), self.Ny // 4 + 10)
+        pygame.draw.circle(self.screen, self.GREEN, (self.CIRCLE_X, self.green_circle_y), self.RADIUS, self.BORDER_WIDTH) # start-spawncircle
+        pygame.draw.circle(self.screen, self.ORANGE, (self.CIRCLE_X, self.orange_circle_y), self.RADIUS, self.BORDER_WIDTH) # target-spawn circle
+        pygame.draw.circle(self.screen, self.BLACK, (int(self.x), int(self.y)), 5) # boat
+        pygame.draw.circle(self.screen, self.RED, (int(self.target_x), int(self.target_y)), self.RADIUS/3) # targe-spawn circle
+        pygame.display.update()
+
+    def baseline_test(self):
         running = True
         while running:
+            
+            self.screen.fill(self.BACKGROUND_COLOR)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            self.screen.fill(self.BACKGROUND_COLOR)
-            ux, uy = self.update_flow_field()
-            self.draw_quiver(ux, uy)
+
+            self.update_flow_field()
+            self.draw_quiver()
             int_x, int_y = int(self.x), int(self.y)
             if 0 <= int_x < self.WIDTH and 0 <= int_y < self.HEIGHT:
-                velocity_x = ux[int_y, int_x] + self.v_x
-                velocity_y = uy[int_y, int_y] + self.v_y
+                velocity_x = self.ux[int_y, int_x] + self.v_x
+                velocity_y = self.uy[int_y, int_y] + self.v_y
                 self.x += velocity_x
                 self.y += velocity_y
+
             pygame.draw.circle(self.screen, self.BLACK, (self.Nx // 4, self.Ny // 2), self.Ny // 4 + 10)
-            pygame.draw.circle(self.screen, self.GREEN, (self.CIRCLE_X, self.green_circle_y), self.RADIUS, self.BORDER_WIDTH)
-            pygame.draw.circle(self.screen, self.ORANGE, (self.CIRCLE_X, self.orange_circle_y), self.RADIUS, self.BORDER_WIDTH)
-            pygame.draw.circle(self.screen, self.BLACK, (int(self.x), int(self.y)), 5)
+            pygame.draw.circle(self.screen, self.GREEN, (self.CIRCLE_X, self.green_circle_y), self.RADIUS, self.BORDER_WIDTH) # start-spawncircle
+            pygame.draw.circle(self.screen, self.ORANGE, (self.CIRCLE_X, self.orange_circle_y), self.RADIUS, self.BORDER_WIDTH) # target-spawn circle
+            pygame.draw.circle(self.screen, self.BLACK, (int(self.x), int(self.y)), 5) # boat
+            pygame.draw.circle(self.screen, self.RED, (int(self.target_x), int(self.target_y)), self.RADIUS/3) # targe-spawn circle
             pygame.display.update()
+
         pygame.quit()
         sys.exit()
 
 if __name__ == "__main__":
-    sim = VortexStreetSimulation()
-    sim.run()
+    sim = VortexENV()
+    sim.baseline_test()
+    pygame.quit()
+    sys.exit()
